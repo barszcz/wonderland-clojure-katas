a3b57678228e59c2d7d9ac1d0541bebcb7b39f4a
Jeffrey Barszcz <jfbarszcz@gmail.com>
2015-03-17 15:10:06 -0400

    First attempt at alphabet cipher solution, could be DRYer

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,29 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.set :refer [map-invert]]))
+
+(def offsets-map (zipmap "abcdefghijklmnopqrstuvwxyz" (range 26)))
+(def letters-map (map-invert offsets-map))
+
+(defn encode-letter [k m]
+  (let [x (get offsets-map k)
+        y (get offsets-map m)]
+    (get letters-map (mod (+ x y) 26))))
+
+(defn decode-letter [k m]
+  (let [x (get offsets-map k)
+        y (get offsets-map m)]
+    (get letters-map (mod (- y x) 26))))
+
+
+(defn code [keyword message func]
+  (let [len (count message)
+        key (take len (cycle keyword))]
+    (->> message
+         (map func key)
+         (apply str))))
 
 (defn encode [keyword message]
-  "encodeme")
+  (code keyword message encode-letter))
 
 (defn decode [keyword message]
-  "decodeme")
+  (code keyword message decode-letter))
